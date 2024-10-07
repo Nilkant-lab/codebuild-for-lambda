a87ddde7ef19377e9bb1217dd456d27f5574b782
Nilkant-lab <70192483+Nilkant-lab@users.noreply.github.com>
2024-10-07 15:52:36 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!...Edit CCM my test.... (Modified v0.2')
+        'body': json.dumps('Hello from Lambda! this is for POC in Cloud session...Edit CCM my test.... (Modified v0.2')
     }
